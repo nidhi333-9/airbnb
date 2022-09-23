14aaacb44a2d271c64860a4165590af64624fc14
nidhikumari <kumari9936nidhi@gmail.com>
2022-09-23 13:31:11 +0530

    modified App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-
-
 import './App.css';
 import Card from './components/Card';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import Data from './data'
 
 function App() {
-
+  const dataElement = Data.map(item => {
+    return <Card img={item.coverImg} rating={item.rating} reviewCount={item.reviewCount} country={item.country} title={item.title} price={item.price} />
+  })
 
   return (
     <div className="App">
       <Navbar />
       <Main />
-      <Card
-        img="../images/img1.png"
+      {dataElement}
+      {/* <Card
+        img="images/img1.png"
         rating="5.0"
         reviewCount={6}
         country="USA"
         title="Life Lessons with Katie Zaferes"
-        price={136} />
+        price={136} /> */}
+
     </div>
   )
 }
